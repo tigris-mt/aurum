bfd18f5a1930df3e8f079d3d8abbd478ae419698
Beha <shacknetisp@mail.com>
2019-07-10 15:04:09 -0400

    Grass.

--- mods/items/aurum_base/base.lua
+++ mods/items/aurum_base/base.lua
@@ -2,6 +2,8 @@ local S = minetest.get_translator()
 
 minetest.register_node("aurum_base:stone", {
 	description = S("Stone"),
+	_doc_items_longdesc = S("An extremely common node. Often surrounds ores."),
+	_doc_items_hidden = false,
 	tiles = {"aurum_base_stone.png"},
 	is_ground_content = true,
 	sounds = aurum.sounds.stone(),
@@ -9,7 +11,18 @@ minetest.register_node("aurum_base:stone", {
 
 minetest.register_node("aurum_base:dirt", {
 	description = S("Dirt"),
+	_doc_items_longdesc = S("Decayed material and bits of rock."),
+	_doc_items_hidden = false,
 	tiles = {"aurum_base_dirt.png"},
 	is_ground_content = true,
 	sounds = aurum.sounds.dirt(),
+})
+
+minetest.register_node("aurum_base:grass", {
+	description = S("Grass"),
+	_doc_items_longdesc = S("Grassy dirt, a mass of plant matter."),
+	_doc_items_hidden = false,
+	tiles = {"aurum_base_grass.png"},
+	is_ground_content = true,
+	sounds = aurum.sounds.grass(),
 })
--- mods/mapgen/aurum_biomes/default_biomes.lua
+++ mods/mapgen/aurum_biomes/default_biomes.lua
@@ -1,9 +1,9 @@
 aurum.biomes.register("aurum:aurum", {
 	name = "aurum_barrens",
-	node_top = "aurum_base:dirt",
-	depth_top = 2,
+	node_top = "aurum_base:grass",
+	depth_top = 1,
 	node_filler = "aurum_base:dirt",
-	depth_filler = 4,
+	depth_filler = 3,
 	heat_point = 50,
 	humidity_point = 50,
 })

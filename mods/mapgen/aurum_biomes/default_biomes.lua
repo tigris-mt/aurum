3e41b9d7041ee8175d2bee8ec3b524e8c52a2834
Beha <shacknetisp@mail.com>
2019-07-10 11:35:31 -0400

    aurum_hud

--- mods/mapgen/aurum_biomes/default_biomes.lua
+++ mods/mapgen/aurum_biomes/default_biomes.lua
@@ -4,15 +4,6 @@ aurum.biomes.register("aurum:aurum", {
 	depth_top = 2,
 	node_filler = "aurum_base:dirt",
 	depth_filler = 4,
-	y_min = -6,
-	heat_point = 50,
-	humidity_point = 50,
-})
-
-aurum.biomes.register("aurum:aurum", {
-	name = "aurum_barrens_underground",
-	node_stone = "aurum_base:stone",
-	y_max = 0,
 	heat_point = 50,
 	humidity_point = 50,
 })

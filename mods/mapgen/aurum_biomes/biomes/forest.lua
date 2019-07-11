33b4c1dd2ddc41b5ed3c175a69203440005a7bce
Beha <shacknetisp@mail.com>
2019-07-11 15:50:43 -0400

    Adjust biome dirt depth.

--- mods/mapgen/aurum_biomes/biomes/forest.lua
+++ mods/mapgen/aurum_biomes/biomes/forest.lua
@@ -3,7 +3,7 @@ aurum.biomes.register("aurum:aurum", {
 	node_top = "aurum_base:grass",
 	depth_top = 1,
 	node_filler = "aurum_base:dirt",
-	depth_filler = 3,
+	depth_filler = 4,
 	heat_point = 50,
 	humidity_point = 50,
 })

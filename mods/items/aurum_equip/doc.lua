b77e8a4e039a52c034771d0f0d5c4ec882b518e3
Beha <shacknetisp@mail.com>
2019-07-16 17:48:12 -0400

    Armor doc.

--- mods/items/aurum_equip/doc.lua
+++ mods/items/aurum_equip/doc.lua
@@ -1,1 +1,19 @@
+local S = minetest.get_translator()
 
+doc.sub.items.register_factoid("tools", "use", function(itemstring, def)
+	if def._eqdef and def._eqdef.armor then
+		local t = {}
+		for k,v in table.spairs(def._eqdef.armor) do
+			table.insert(t, S(k .. ": @1%", 100 - math.floor(v * 100)))
+		end
+		return S("By itself, this armor provides protection: @1", table.concat(t, ", "))
+	end
+	return ""
+end)
+
+doc.sub.items.register_factoid("tools", "use", function(itemstring, def)
+	if def._eqdef and def._eqdef.durability then
+		return S("By itself, this equipment lasts around @1 uses.", def._eqdef.durability)
+	end
+	return ""
+end)

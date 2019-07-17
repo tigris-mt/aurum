670115014c9c185d8578cc3d1bb173e378f1f037
Beha <shacknetisp@mail.com>
2019-07-17 12:22:50 -0400

    psyche shield enchantment

--- mods/items/aurum_equip/armor_enchants.lua
+++ mods/items/aurum_equip/armor_enchants.lua
@@ -10,3 +10,14 @@ aurum.tools.register_enchant("speed", {
 		state.eqdef.effects.speed = (state.eqdef.effects.speed or 1) * (1 + (level + 1) / 5)
 	end,
 })
+
+aurum.tools.register_enchant("psyche_shield", {
+	categories = {
+		helmet = true,
+	},
+	description = S"Psyche Shield",
+	apply = function(state, level)
+		state.eqdef.armor = table.copy(state.eqdef.armor) or {}
+		state.eqdef.armor.psyche = (state.eqdef.armor.psyche or 1) / (1 + level / 3)
+	end,
+})

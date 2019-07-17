6e77c7583f3214e107a0a861b75df4e3dc3690f9
Beha <shacknetisp@mail.com>
2019-07-17 12:13:49 -0400

    Adjust speed enchantment.

--- mods/items/aurum_equip/armor_enchants.lua
+++ mods/items/aurum_equip/armor_enchants.lua
@@ -7,6 +7,6 @@ aurum.tools.register_enchant("speed", {
 	description = S"Speed",
 	apply = function(state, level)
 		state.eqdef.effects = state.eqdef.effects or {}
-		state.eqdef.effects.speed = (state.eqdef.effects.speed or 1) * (1 + level / 10)
+		state.eqdef.effects.speed = (state.eqdef.effects.speed or 1) * (1 + (level + 1) / 5)
 	end,
 })

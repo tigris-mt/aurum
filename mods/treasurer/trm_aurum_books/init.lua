382c018a7bc3a252e6949064d76e88060845130e
Beha <shacknetisp@mail.com>
2019-08-03 11:01:06 -0400

    trum_aurum_books.random_book(group) returns data, item

--- mods/treasurer/trm_aurum_books/init.lua
+++ mods/treasurer/trm_aurum_books/init.lua
@@ -5,6 +5,7 @@ trm_aurum_books.groups = {}
 function trm_aurum_books.register(def)
 	local def = table.combine({
 		weight = 1,
+		items = {"aurum_books:book_written"},
 		callback = function(data)
 			return data
 		end,
@@ -13,6 +14,7 @@ function trm_aurum_books.register(def)
 			lore = true,
 			lorebook = true,
 		}, def.groups or {}),
+
 		data = table.combine({
 			author = nil,
 			title = nil,
@@ -27,12 +29,31 @@ function trm_aurum_books.register(def)
 	end
 end
 
+function trm_aurum_books.random_book(group)
+	local entries = trm_aurum_books.groups[group]
+	if not entries then
+		return
+	end
+
+	local choice = aurum.weighted_choice(entries)
+
+	local item = nil
+	if #choice.items > 0 then
+		item = choice.items[math.random(#choice.items)]
+	end
+
+	return choice.callback(table.copy(choice.data)), item
+end
+
 minetest.register_on_mods_loaded(function()
 	local len = #table.keys(trm_aurum_books.groups)
-	for g,entries in pairs(trm_aurum_books.groups) do
+	for g in pairs(trm_aurum_books.groups) do
 		aurum.treasurer.register_itemstack_callback(ItemStack("aurum_books:book_written"), function(stack)
-			local choice = aurum.weighted_choice(entries)
-			return gtextitems.set_item(stack, choice.callback(table.copy(choice.data)))
+			local data, item = trm_aurum_books.random_book(g)
+			if item then
+				stack:set_name(item)
+			end
+			return gtextitems.set_item(stack, data)
 		end, 0.5 / len, 3, 1, 0, g)
 	end
 end)

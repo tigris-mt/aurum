fd83311af0fade05251792173a66ab920f905264
Beha <shacknetisp@mail.com>
2019-08-06 16:53:08 -0400

    More names.

--- mods/env/aurum_flavor/names.lua
+++ mods/env/aurum_flavor/names.lua
@@ -3,19 +3,37 @@ local syllables = {
 	"a", "bel", "cin", "don", "el", "fra", "gle", "hy", "kle", "lia", "mae", "nol", "ori", "prai", "ro", "sve", "tri", "ux", "vim", "xaxa", "zad",
 }
 
+local vowels = "aeiouy"
+local consonants = "bcdfghjklmnpqrstvwxyz"
+
 local function choose(list)
 	return list[math.random(#list)]
+end
+
+local function schoose(str)
+	local i = math.random(#str)
+	return str:sub(i, i)
 end
 
 local function capitalize(s)
 	return s:sub(1, 1):upper() .. s:sub(2)
 end
 
+local function syllable()
+	return (aurum.weighted_choice{
+		{function() return choose(syllables) end, 10},
+		{function() return schoose(vowels) end, 1},
+		{function() return schoose(consonants) .. schoose(vowels) end, 1},
+		{function() return schoose(vowels) .. schoose(vowels) end, 1},
+		{function() return schoose(consonants) .. schoose(vowels) .. schoose(vowels) end, 1},
+	})()
+end
+
 local function single()
-	return capitalize((choose{
-		function() return choose(syllables) .. "-" .. choose(syllables) end,
-		function() return choose(syllables) .. choose(syllables) end,
-		function() return choose(syllables) .. choose(syllables) .. choose(syllables) end,
+	return capitalize((aurum.weighted_choice{
+		{function() return syllable() .. "-" .. syllable() end, 0.25},
+		{function() return syllable() .. syllable() end, 1},
+		{function() return syllable() .. syllable() .. syllable() end, 1},
 	})())
 end
 

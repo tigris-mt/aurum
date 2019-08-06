a9ed296dbc7b67c5b9d7855f6fe7fe36e3610197
Beha <shacknetisp@mail.com>
2019-08-06 12:11:49 -0400

    Advanced names.

--- mods/core/aurum_flavor/names.lua
+++ mods/core/aurum_flavor/names.lua
@@ -1,5 +1,6 @@
 local syllables = {
 	"ae", "bo", "ce", "da", "e", "fi", "gu", "ha", "je", "ko", "lu", "me", "ni", "o", "pe", "qu", "ra", "se", "ti", "u", "vo", "xy", "y", "zu",
+	"a", "bel", "cin", "don", "el", "fra", "gle", "hy", "kle", "lia", "mae", "nol", "ori", "prai", "ro", "sve", "tri", "ux", "vim", "xaxa", "zad",
 }
 
 local function choose(list)
@@ -10,6 +11,19 @@ local function capitalize(s)
 	return s:sub(1, 1):upper() .. s:sub(2)
 end
 
+function single()
+	return capitalize((choose{
+		function() return choose(syllables) .. "-" .. choose(syllables) end,
+		function() return choose(syllables) .. choose(syllables) end,
+		function() return choose(syllables) .. choose(syllables) .. choose(syllables) end,
+	})())
+end
+
 function aurum.flavor.generate_name()
-	return capitalize(choose(syllables) .. choose(syllables) .. choose(syllables)) .. " " .. capitalize(choose(syllables) .. choose(syllables) .. choose(syllables))
+	return (choose{
+		function() return single() .. " of " .. single() end,
+		function() return single() .. " " .. single() end,
+		function() return single() .. " " .. single() .. " " .. single() end,
+		function() return single() .. " " .. single() end,
+	})()
 end

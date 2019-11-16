13a3849cd54ce73342ef86347a727e048e155e02
Beha <shacknetisp@mail.com>
2019-11-16 13:45:03 -0500

    gequip handles delegation

--- mods/player/aurum_gui/inventory.lua
+++ mods/player/aurum_gui/inventory.lua
@@ -26,65 +26,25 @@ sfinv.override_page("sfinv:crafting", {
 	end,
 })
 
-local DELEGATE = "aurum_player_delegate"
-
-minetest.register_on_joinplayer(function(player)
-	player:get_inventory():set_size(DELEGATE, 1)
-	-- Ensure delegate inventory is clear.
-	player:get_inventory():set_list(DELEGATE, {})
-end)
-
-minetest.register_allow_player_inventory_action(function(player, action, inv, info)
-	local stack
-	if action == "move" and info.to_list == DELEGATE and info.from_list ~= DELEGATE then
-		stack = ItemStack(player:get_inventory():get_list(info.from_list)[info.from_index])
-		stack:set_count(info.count)
-	elseif action == "put" and info.listname == DELEGATE then
-		stack = info.stack
-	else
-		return nil
-	end
-
-	for type,def in pairs(gequip.types) do
-		if player:get_inventory():room_for_item(def.list_name, stack) and (def.allow_player_inventory_action(player, "put", player:get_inventory(), {stack = stack, listname = def.list_name}) or 1) > 0 then
-			return 1
-		end
-	end
-
-	return 0
-end)
-
-minetest.register_on_player_inventory_action(function(player, action, inv, info)
-	local stack
-	if action == "move" and info.to_list == DELEGATE then
-		stack = ItemStack(player:get_inventory():get_list(info.to_list)[info.to_index])
-		stack:set_count(info.count)
-	elseif action == "put" and info.listname == DELEGATE then
-		stack = info.stack
-	else
-		return
-	end
-
-	local def = gequip.types[stack:get_definition()._eqtype]
-	player:get_inventory():set_list(DELEGATE, {})
-	player:get_inventory():add_item(def.list_name, stack)
-	def.on_player_inventory_action(player, "put", player:get_inventory(), {stack = stack, listname = def.list_name})
-end)
-
 local form = smartfs.create("aurum_player:equipment", function(state)
     state:size(8, 6)
 
+	local inventories = ""
+	local listrings = ""
 	local function i(x, y, n)
-		state:inventory(x, y, 1, 1, n)
-		state:element("code", {name = "listring_" .. n, code = "listring[current_player;main]listring[current_player;" .. n .. "]"})
+		inventories = inventories .. "list[current_player;" .. n .. ";" .. x .. "," .. y .. ";1,1]"
+		listrings = listrings .. "listring[current_player;main]listring[current_player;" .. n .. "]"
 	end
-	i(3.5, 0, "gequip_head")
-	i(3.5, 1, "gequip_chest")
-	i(3.5, 2, "gequip_legs")
-	i(3.5, 3, "gequip_feet")
-	-- Invisible inventory list for shift-insertion.
-	-- Must be last inventory in the listring.
-	i(-2, -2, DELEGATE)
+
+	-- Invisible delegate inventory.
+	-- Must be first inventory in the listring.
+	i(-2, -2, gequip.DELEGATE)
+	i(3.5, 0, gequip.types["head"].list_name)
+	i(3.5, 1, gequip.types["chest"].list_name)
+	i(3.5, 2, gequip.types["legs"].list_name)
+	i(3.5, 3, gequip.types["feet"].list_name)
+
+	state:element("code", {name = "listrings", code = inventories .. "\n" .. listrings})
 
 	local function g(n)
 		return S(n .. ": @1%", 100 - minetest.get_player_by_name(state.location.player):get_armor_groups()[n])

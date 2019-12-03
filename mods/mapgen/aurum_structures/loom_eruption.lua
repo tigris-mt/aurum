9e85ac054ef9662e94d139736b482743df6c4ad0
Beha <shacknetisp@mail.com>
2019-12-03 10:30:12 -0500

    cache loom eruptions

--- mods/mapgen/aurum_structures/loom_eruption.lua
+++ mods/mapgen/aurum_structures/loom_eruption.lua
@@ -1,27 +1,33 @@
 -- An eruption of regrets from the loom.
 -- Occurs in all barrens.
 
+local cache = {}
+
 aurum.features.register_decoration{
 	place_on = {"aurum_base:regret", "aurum_base:stone", "aurum_base:gravel"},
 	rarity = 0.001,
 	biomes = aurum.biomes.get_all_group("barren"),
 
 	make_schematic = function(pos, random)
-		local size = vector.new(random(1, 4), random(4, 18), random(1, 4))
+		local size = vector.new(random(1, 8), random(4, 18), random(1, 8))
+		local hash = minetest.hash_node_position(size)
 		local limit = vector.subtract(size, 1)
 
 		pos.y = pos.y - math.floor(limit.y / 2)
 
-		local area = aurum.box.voxelarea(aurum.box.new_add(vector.new(0, 0, 0), limit))
-		local data = {}
+		if not cache[hash] then
+			local data = {}
+			local area = aurum.box.voxelarea(aurum.box.new_add(vector.new(0, 0, 0), limit))
 
-		for i in area:iterp(vector.new(0, 0, 0), limit) do
-			data[i] = {name = "aurum_base:regret", prob = 64}
+			for i in area:iterp(vector.new(0, 0, 0), limit) do
+				data[i] = {name = "aurum_base:regret", prob = 64}
+			end
+			cache[hash] = data
 		end
 
 		return {
 			size = size,
-			data = data,
+			data = cache[hash],
 		}
 	end,
 }

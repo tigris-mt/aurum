9dd9edec7b0ab845dd79de6f4d826572b42bcb14
Beha <shacknetisp@mail.com>
2020-03-23 23:25:07 -0400

    Bounce and less fall damage on aether skin and flesh.

--- mods/items/aurum_base/base.lua
+++ mods/items/aurum_base/base.lua
@@ -93,7 +93,7 @@ doc.add_entry_alias("nodes", "aurum_base:ice", "nodes", "aurum_base:ground_ice")
 
 minetest.register_node("aurum_base:aether_shell", {
 	description = S"Aether Shell",
-	_doc_items_longdesc = S"A hard organic material interlaced with old magic.",
+	_doc_items_longdesc = S"Hard organic material interlaced with old magic. Remarkably similar to fragment of snail shell.",
 	tiles = {"aurum_base_aether_shell.png"},
 	sounds = aurum.sounds.stone(),
 	groups = {dig_pick = 2, level = 2},
@@ -101,10 +101,10 @@ minetest.register_node("aurum_base:aether_shell", {
 
 minetest.register_node("aurum_base:aether_skin", {
 	description = S"Aether Skin",
-	_doc_items_longdesc = S"A tough, leathery skin.",
+	_doc_items_longdesc = S"Tough, leathery skin.",
 	tiles = {"aurum_base_aether_skin.png"},
 	sounds = aurum.sounds.flesh(),
-	groups = {dig_dig = 2, dig_chop = 2, level = 2},
+	groups = {dig_dig = 2, dig_chop = 2, level = 2, bouncy = 80, fall_damage_add_percent = -30},
 })
 
 minetest.register_node("aurum_base:aether_flesh", {
@@ -112,7 +112,7 @@ minetest.register_node("aurum_base:aether_flesh", {
 	_doc_items_longdesc = S"Strange flesh.",
 	tiles = {"aurum_base_aether_flesh.png"},
 	sounds = aurum.sounds.flesh(),
-	groups = {dig_dig = 2, dig_chop = 2},
+	groups = {dig_dig = 2, dig_chop = 2, slippery = 2, fall_damage_add_percent = -70, bouncy = 20},
 	paramtype = "light",
 	light_source = 2,
 })

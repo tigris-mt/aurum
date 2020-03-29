d8fe5cf6623239c1b38086a15f9765a1458eee81
Beha <shacknetisp@mail.com>
2020-03-29 19:24:01 -0400

    b colors for clay

--- mods/items/aurum_clay/init.lua
+++ mods/items/aurum_clay/init.lua
@@ -1,11 +1,11 @@
 local S = minetest.get_translator()
 
 for _,row in ipairs(dye.dyes) do
-	local dye = {name = row[1], desc = row[2], colorspec = row[3]}
+	local dye = {name = row[1], desc = row[2], colorspec = b.color.convert(row[1])}
 
 	minetest.register_node("aurum_clay:" .. dye.name, {
 		description = S("@1 Clay", dye.desc),
-		tiles = {"aurum_clay_clay.png^[colorize:" .. dye.colorspec .. ":200"},
+		tiles = {"aurum_clay_clay.png^[colorize:" .. b.color.tostring(dye.colorspec) .. ":200"},
 		groups = {dig_dig = 3, clay = 1},
 		sounds = aurum.sounds.dirt(),
 	})

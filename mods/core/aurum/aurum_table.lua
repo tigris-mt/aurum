65452f7ab426cb4acf854d6e84766adce9d00822
Beha <shacknetisp@mail.com>
2020-04-02 10:00:13 -0400

    aurum.MT_VERSION

--- mods/core/aurum/aurum_table.lua
+++ mods/core/aurum/aurum_table.lua
@@ -1,8 +1,10 @@
 -- This file must not reference any other file.
 -- It can be read by external lua interpreters.
 
--- Create the aurum table.
+-- Create the aurum table and populate it with metadata.
 aurum = {
 	-- Current version number.
 	VERSION = "0.44.2",
+	-- Expected Minetest Engine version (>=).
+	MT_VERSION = "5.2.0",
 }

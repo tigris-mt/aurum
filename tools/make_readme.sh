1a5d980a666e19ce78acbfbb38c96126a2aff37d
Beha <shacknetisp@mail.com>
2020-04-03 11:08:18 -0400

    Fix name of pandoc tool.

--- tools/make_readme.sh
+++ tools/make_readme.sh
@@ -3,4 +3,4 @@ set -e
 . tools/config.sh
 
 eval "echo \"$(cat docs/README.sh.md)\"" > README.md
-pandoc -f markdown_github -t tools/pandoc_bbcode_phbb.lua -o README.bbcode README.md
+pandoc -f markdown_github -t tools/pandoc_bbcode_phpbb.lua -o README.bbcode README.md

c29d97b039816acc738798f9d50e16ca7ed02694
Beha <shacknetisp@mail.com>
2020-04-03 19:24:22 -0400

    Spawn limiting applies only to mobs of the same type.

--- mods/entities/aurum_mobs/spawning.lua
+++ mods/entities/aurum_mobs/spawning.lua
@@ -1,7 +1,7 @@
 local RADIUS = 40
 local TIMER = 1
--- Maximum mobs in <RADIUS> around a player for a new mob to spawn.
-local SPAWN_LIMIT = tonumber(minetest.settings:get("aurum.mobs.spawn_limit")) or 6
+-- Maximum mobs of the same type in <RADIUS> around a player for a new mob to spawn.
+local SPAWN_LIMIT = tonumber(minetest.settings:get("aurum.mobs.spawn_limit")) or 4
 
 aurum.mobs.spawns = {}
 aurum.mobs.spawn_biomes = {}
@@ -44,16 +44,6 @@ function aurum.mobs.register_spawn(def)
 	table.insert(aurum.mobs.spawns, def)
 end
 
-local function mobs_in_radius(pos, radius)
-	local ret = 0
-	for _,object in ipairs(minetest.get_objects_inside_radius(pos, radius)) do
-		if object:get_luaentity() and object:get_luaentity()._aurum_mobs_id then
-			ret = ret + 1
-		end
-	end
-	return ret
-end
-
 -- Mob limit of zero means no spawning.
 if SPAWN_LIMIT ~= 0 then
 	local timer = 0
@@ -64,24 +54,27 @@ if SPAWN_LIMIT ~= 0 then
 				local pos = player:get_pos()
 				local biome = minetest.get_biome_data(pos)
 				local biome_name = biome and minetest.get_biome_name(biome.biome)
+				local nearby_mobs = b.t.imap(minetest.get_objects_inside_radius(pos, RADIUS), function(v) return (v:get_luaentity() and v:get_luaentity()._aurum_mobs_id) and v or nil end)
 
-				if biome_name and (SPAWN_LIMIT < 0 or mobs_in_radius(pos, RADIUS) <= SPAWN_LIMIT) then
+				if biome_name then
 					for _,def in ipairs(aurum.mobs.spawn_biomes[biome_name] or {}) do
-						def.timer = def.timer - timer
-						if def.timer <= 0 then
-							local spawned = false
-							local box = b.box.new_radius(pos, RADIUS)
-							for _,pos in b.t.ro_ipairs(minetest.find_nodes_in_area_under_air(box.a, box.b, def.nodes)) do
-								local spawn_pos = vector.add(pos, vector.new(0, 1, 0))
-								local light = minetest.get_node_light(spawn_pos)
-								if light >= def.light_min and light <= def.light_max and math.random() < (1 / def.chance) then
-									if aurum.mobs.spawn(spawn_pos, def.mob) then
-										spawned = true
-										minetest.log("action", ("Spawned mob %s at %s near %s"):format(def.mob, minetest.pos_to_string(spawn_pos), player:get_player_name()))
+						if (SPAWN_LIMIT < 0 or #b.t.imap(nearby_mobs, function(v) return (v:get_luaentity().name == def.mob) and v or nil end) <= SPAWN_LIMIT) then
+							def.timer = def.timer - timer
+							if def.timer <= 0 then
+								local spawned = false
+								local box = b.box.new_radius(pos, RADIUS)
+								for _,pos in b.t.ro_ipairs(minetest.find_nodes_in_area_under_air(box.a, box.b, def.nodes)) do
+									local spawn_pos = vector.add(pos, vector.new(0, 1, 0))
+									local light = minetest.get_node_light(spawn_pos)
+									if light >= def.light_min and light <= def.light_max and math.random() < (1 / def.chance) then
+										if aurum.mobs.spawn(spawn_pos, def.mob) then
+											spawned = true
+											minetest.log("action", ("Spawned mob %s at %s near %s"):format(def.mob, minetest.pos_to_string(spawn_pos), player:get_player_name()))
+										end
 									end
 								end
+								reset_timer(def, (not spawned) and 0.1)
 							end
-							reset_timer(def, (not spawned) and 0.1)
 						end
 					end
 				end

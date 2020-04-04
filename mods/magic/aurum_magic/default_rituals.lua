7edc8064fe8f4710edc363d9ef8223d7d0330d5f
Beha <shacknetisp@mail.com>
2020-04-04 12:48:10 -0400

    Require bananas in growth spells. Closes #18

--- mods/magic/aurum_magic/default_rituals.lua
+++ mods/magic/aurum_magic/default_rituals.lua
@@ -33,14 +33,14 @@ for _,pos in ipairs(b.box.poses(b.box.new(vector.new(-1, 0, -1), vector.new(1, 2
 	table.insert(fertilizer_wall, {pos, "aurum_farming:fertilizer"})
 end
 aurum.magic.register_spell_ritual("growth", {
-	longdesc = S"Extracts the primal energy of fertilizer and burn it into spell scrolls of growth.",
+	longdesc = S"Extracts the primal energy of fertilizer and burns it into spell scrolls of growth. Requires bananas to appease the elements.",
 
 	size = b.box.new(vector.new(-1, 0, -1), vector.new(1, 2, 0)),
 
 	recipe = b.t.icombine(fertilizer_wall, {
 		{vector.new(0, 1, 0), "aurum_storage:scroll_hole"},
-		{vector.new(-1, 0, 0), "aurum_flare:flare"},
-		{vector.new(1, 0, 0), "aurum_flare:flare"},
+		{vector.new(-1, 0, 0), "aurum_trees:banana"},
+		{vector.new(1, 0, 0), "aurum_trees:banana"},
 	}),
 
 	protected = true,

1eb45c4153fc547a1aa3f254dc483a0face8b395
Beha <shacknetisp@mail.com>
2020-04-05 12:22:42 -0400

    Disable the join award sound.

--- mods/player/aurum_awards/awards/join.lua
+++ mods/player/aurum_awards/awards/join.lua
@@ -3,6 +3,7 @@ local S = minetest.get_translator()
 awards.register_award("aurum_awards:join", {
 	title = S"The Titan Cometh in Power",
 	description = S"Make your arrival in the world of Aurum.",
+	sound = false,
 })
 
 minetest.register_on_joinplayer(function(player)

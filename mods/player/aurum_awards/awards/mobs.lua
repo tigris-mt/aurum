07fd6aa2bf9a6e8c623ece84a92c30100228b235
Beha <shacknetisp@mail.com>
2020-04-04 22:25:19 -0400

    Add mower and reaper awards.

--- mods/player/aurum_awards/awards/mobs.lua
+++ mods/player/aurum_awards/awards/mobs.lua
@@ -8,3 +8,23 @@ awards.register_award("aurum_awards:first_blood", {
 		target = 1,
 	},
 })
+
+awards.register_award("aurum_awards:mower", {
+	title = S"Mower",
+	requires = {"aurum_awards:first_blood"},
+	description = S"Smite 100 mobs.",
+	trigger = {
+		type = "mob_kill",
+		target = 100,
+	},
+})
+
+awards.register_award("aurum_awards:reaper", {
+	title = S"The Grim Reaper",
+	requires = {"aurum_awards:mower"},
+	description = S"Destroy 1000 mobs.",
+	trigger = {
+		type = "mob_kill",
+		target = 1000,
+	},
+})

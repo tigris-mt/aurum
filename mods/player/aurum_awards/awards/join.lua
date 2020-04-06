64bd91cf4c8cfae2cd0756b95bebdb431e29ac0a
Beha <shacknetisp@mail.com>
2020-04-06 17:25:04 -0400

    Rephrase join award.

--- mods/player/aurum_awards/awards/join.lua
+++ mods/player/aurum_awards/awards/join.lua
@@ -1,8 +1,8 @@
 local S = minetest.get_translator()
 
 awards.register_award("aurum_awards:join", {
-	title = S"The Titan Cometh in Power",
-	description = S"Make your arrival in the world of Aurum.",
+	title = S"The Titan is Here",
+	description = S"Arrive in the world of Aurum.",
 	sound = false,
 })
 

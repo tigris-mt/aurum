6decb6d92b849e19ccfbc9e7f4b20cffa197d786
Beha <shacknetisp@mail.com>
2020-04-08 14:45:47 -0400

    Go Home ritual requires full health.

--- mods/magic/aurum_magic/rituals/home.lua
+++ mods/magic/aurum_magic/rituals/home.lua
@@ -2,7 +2,7 @@ local S = minetest.get_translator()
 
 aurum.magic.register_ritual("aurum_rituals:home", {
 	description = S"Go Home",
-	longdesc = S"Return to your respawn point.\nThis ritual can only be performed in Aurum.",
+	longdesc = S"Return to your respawn point.\nThis ritual can only be performed in Aurum, and you will must have full health.",
 
 	size = b.box.new(vector.new(-1, -1, -1), vector.new(1, 0, 1)),
 	protected = false,
@@ -23,6 +23,9 @@ aurum.magic.register_ritual("aurum_rituals:home", {
 		-- Require Aurum.
 		if screalms.pos_to_realm(at(vector.new(0, 0, 0))) ~= "aurum:aurum" then
 			return false, S"Only the energies of Aurum could support this ritual."
+		end
+		if player:get_hp() < player:get_properties().hp_max then
+			return false, S"You are not healthy enough to teleport."
 		end
 		return (aurum.player.spawn_totem(player) or aurum.player.spawn_realm(player)), S"There is nowhere for you to go."
 	end,

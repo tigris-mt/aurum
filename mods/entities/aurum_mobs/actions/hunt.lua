d7c26952261579daf31360f3040cf2be0727fc63
Beha <shacknetisp@mail.com>
2020-04-08 11:01:54 -0400

    Fix angler.

--- mods/entities/aurum_mobs/actions/hunt.lua
+++ mods/entities/aurum_mobs/actions/hunt.lua
@@ -1,6 +1,9 @@
+-- Empty table means all is valid.
+aurum.mobs.initial_data.hunt_prey = {}
+
 gemai.register_action("aurum_mobs:find_prey", function(self)
 	for _,object in b.t.ro_ipairs(minetest.get_objects_inside_radius(self.entity.object:get_pos(), aurum.mobs.SEARCH_RADIUS)) do
-		if object:get_luaentity() and object:get_luaentity()._aurum_mobs_id and object:get_luaentity().name ~= self.entity.name and aurum.mobs.helper_can_see(self, object) then
+		if object:get_luaentity() and object:get_luaentity()._aurum_mobs_id and object:get_luaentity().name ~= self.entity.name and aurum.mobs.helper_can_see(self, object) and ((#self.data.hunt_prey == 0) or aurum.match_item_list(object:get_luaentity().name, self.data.hunt_prey)) then
 			local mob = object:get_luaentity()._gemai
 			if mob.data.xmana < self.data.xmana then
 				self:fire_event("found_prey", {
@@ -11,7 +14,7 @@ gemai.register_action("aurum_mobs:find_prey", function(self)
 				})
 				return
 			end
-		elseif object:is_player() and aurum.mobs.helper_can_see(self, object) then
+		elseif object:is_player() and aurum.mobs.helper_can_see(self, object) and ((#self.data.hunt_prey == 0) or aurum.match_item_list("player", self.data.hunt_prey)) then
 			self:fire_event("found_prey", {
 				target = {
 					type = "ref_table",

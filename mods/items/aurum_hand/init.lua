f3a2099a77ebab145b9cda393b776d334def7867
Beha <shacknetisp@mail.com>
2020-04-09 11:00:21 -0400

    Update submodules.

---
+++ mods/items/aurum_hand/init.lua
@@ -0,0 +1,53 @@
+local S = minetest.get_translator()
+
+aurum_hand = {}
+
+local function hand_dig(time)
+	return {times = {[3] = time}, uses = 0, maxlevel = 0}
+end
+
+aurum_hand.def = {
+	wield_image = "wieldhand.png",
+	wield_scale = vector.new(1, 1, 2.5),
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level = 0,
+		groupcaps = {
+			dig_handle = {
+				times = {
+					[1] = 2,
+					[2] = 1.5,
+					[3] = 0.5,
+				},
+				uses = 0,
+				maxlevel = 3,
+			},
+			dig_long_handle = {
+				times = {
+					[1] = 15,
+					[2] = 10,
+					[3] = 5,
+				},
+				uses = 0,
+				maxlevel = 3,
+			},
+			-- The hand can dig the lowest levels of all other groups, but slowly.
+			dig_pick = hand_dig(6),
+			dig_chop = hand_dig(3),
+			dig_dig = hand_dig(1.5),
+			dig_snap = hand_dig(0.5),
+		},
+		damage_groups = {
+			impact = 1,
+		},
+	},
+
+	groups = {not_in_creative_inventory = 1},
+
+	_enchant_levels = 2,
+	_enchants = {"hand"},
+}
+
+minetest.register_tool(":", b.t.combine(aurum_hand.def, {
+	type = "none",
+}))
--- mods/items/aurum_tools/default_tools.lua
+++ mods/items/aurum_tools/default_tools.lua
@@ -1,58 +1,11 @@
 local S = minetest.get_translator()
 
-local function handdig(time)
-	return {times = {[3] = time}, uses = 0, maxlevel = 0}
-end
-
-local handdef = aurum.tools.register("aurum_tools:hand", {
+aurum.tools.register("aurum_tools:hand", b.t.combine(aurum_hand.def, {
 	description = S"Hand",
 	_doc_items_longdesc = S"Tools were invented by the primates to surpass the limits of their own limbs, but your hand is not useless on its own. It can dig many nodes with no enhancements at all.",
-	wield_image = "wieldhand.png",
-	wield_scale = vector.new(1, 1, 2.5),
-	tool_capabilities = {
-		full_punch_interval = 0.9,
-		max_drop_level = 0,
-		groupcaps = {
-			dig_handle = {
-				times = {
-					[1] = 2,
-					[2] = 1.5,
-					[3] = 0.5,
-				},
-				uses = 0,
-				maxlevel = 3,
-			},
-			dig_long_handle = {
-				times = {
-					[1] = 15,
-					[2] = 10,
-					[3] = 5,
-				},
-				uses = 0,
-				maxlevel = 3,
-			},
-			-- The hand can dig the lowest levels of all other groups, but slowly.
-			dig_pick = handdig(6),
-			dig_chop = handdig(3),
-			dig_dig = handdig(1.5),
-			dig_snap = handdig(0.5),
-		},
-		damage_groups = {
-			impact = 1,
-		},
-	},
-
-	groups = {not_in_creative_inventory = 1},
-
 	_enchant_levels = 2,
 	_enchants = {"hand"},
-})
-
--- Register the standard hand with the same def as our hand.
-local nhd = table.copy(handdef)
-nhd.type = "none"
-nhd.description = nil
-minetest.register_tool(":", nhd)
+}))
 
 local function register(type, name, texture, def)
 	local odef = {

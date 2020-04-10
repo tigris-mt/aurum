63da711df41183828aeab73c380516117d35d322
Beha <shacknetisp@mail.com>
2020-04-10 17:57:40 -0400

    Full set award.

--- mods/player/aurum_awards/awards/equip.lua
+++ mods/player/aurum_awards/awards/equip.lua
@@ -1,5 +1,12 @@
 local S = minetest.get_translator()
 
+awards.register_award("aurum_awards:full_set", {
+	title = S"Complete Coverage",
+	description = S"Equip your head, chest, legs, and feet slots as the same time.",
+	icon = minetest.registered_items["aurum_equip:iron_hauberk"].inventory_image,
+	difficulty = 75,
+})
+
 awards.register_award("aurum_awards:wings", {
 	title = S"Rival of the Archons",
 	description = S"Equip a pair of wings.",
@@ -10,13 +17,20 @@ awards.register_award("aurum_awards:wings", {
 local old = gequip.refresh
 function gequip.refresh(player)
 	local ret = old(player)
+	local slots = b.set{}
 	local items = b.set{}
 	for k,v in pairs(gequip.types) do
 		for _,item in ipairs(player:get_inventory():get_list(v.list_name)) do
-			items[item:get_name()] = true
+			if item:get_count() > 0 then
+				items[item:get_name()] = true
+				slots[k] = true
+			end
 		end
 	end
-	if items["aurum_wings:wings"] then
+	if slots.head and slots.chest and slots.legs and slots.feet then
+		awards.unlock(player:get_player_name(), "aurum_awards:full_set")
+	end
+	if items["aurum_wings:wings"] or items["aurum_wings:wings_enchanted"] then
 		awards.unlock(player:get_player_name(), "aurum_awards:wings")
 	end
 	return ret

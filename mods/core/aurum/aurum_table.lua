fdb589667627bb9f3b4461a7c04a2915c34e376f
Beha <shacknetisp@mail.com>
2020-04-12 14:11:14 -0400

    Explain version scheme.

--- mods/core/aurum/aurum_table.lua
+++ mods/core/aurum/aurum_table.lua
@@ -5,6 +5,10 @@
 aurum = {
 	-- Current version number.
 	-- MAJOR.ITERATION.MINOR.PATCH
+	--- MAJOR is incremented for incompatible changes.
+	--- ITERATION is incremented for milestone releases.
+	--- MINOR is incremented for new features.
+	--- PATCH is incremented for minor changes and bugfixes.
 	VERSION = "0.1.52.1",
 	-- Expected Minetest Engine version (>=).
 	MT_VERSION = "5.2.0",

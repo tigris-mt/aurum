264a93c40ac3f408f109c7c0c2efae6c1cb0e78c
Beha <shacknetisp@mail.com>
2020-04-14 12:06:59 -0400

    Increment version to 0.53.0.0
    This is so that the iteration number will still come after older
    releases without the iteration number. The iteration will be reset to 0
    upon major version 1 release.

--- mods/core/aurum/aurum_table.lua
+++ mods/core/aurum/aurum_table.lua
@@ -9,7 +9,7 @@ aurum = {
 	--- ITERATION is incremented for milestone releases.
 	--- MINOR is incremented for new features.
 	--- PATCH is incremented for minor changes and bugfixes.
-	VERSION = "0.1.52.1",
+	VERSION = "0.53.0.0",
 	-- Expected Minetest Engine version (>=).
 	MT_VERSION = "5.2.0",
 }

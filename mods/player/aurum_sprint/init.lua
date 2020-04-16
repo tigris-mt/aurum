14b056c2a985c5b704e122dde54caa1cf3b3fd5f
Beha <shacknetisp@mail.com>
2020-04-15 21:01:32 -0400

    Add sprinting.

---
+++ mods/player/aurum_sprint/init.lua
@@ -0,0 +1,6 @@
+minetest.register_globalstep(function()
+	for _,player in ipairs(minetest.get_connected_players()) do
+		player_monoids.speed:add_change(player, player:get_player_control().aux1 and 2.5 or 1, "aurum_sprint:sprint")
+		aurum.hunger.LOSS:add_change(player, player:get_player_control().aux1 and 30 or 1, "aurum_sprint:sprint")
+	end
+end)

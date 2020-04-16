20e919f337ec64db81203dd75121af29bb4ad6f3
Beha <shacknetisp@mail.com>
2020-04-16 09:30:50 -0400

    Add global unlock message.

--- mods/player/aurum_awards/init.lua
+++ mods/player/aurum_awards/init.lua
@@ -1,3 +1,9 @@
+local S = minetest.get_translator()
+
 aurum.awards = {}
+
+awards.register_on_unlock(function(name, def)
+	minetest.chat_send_all(S("@1 unlocked @2", minetest.colorize("#00aaff", name), minetest.colorize("#ffaa00", def.title)))
+end)
 
 b.dodir("awards")

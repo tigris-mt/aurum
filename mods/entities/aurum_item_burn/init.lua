54a4b687bf20855577d74e7797ca8665ed81c96e
Beha <shacknetisp@mail.com>
2020-04-21 13:01:11 -0400

    item burn failsafe

--- mods/entities/aurum_item_burn/init.lua
+++ mods/entities/aurum_item_burn/init.lua
@@ -4,6 +4,9 @@ minetest.registered_entities["__builtin:item"].on_step = function(self, ...)
 	old(self, ...)
 
 	local pos = self.object:get_pos()
+	if not pos then
+		return
+	end
 	local node = minetest.get_node(vector.add(pos, vector.new(0, self.object:get_properties().collisionbox[2] - 0.05, 0)))
 
 	if minetest.get_item_group(node.name, "item_burn") > 0 then

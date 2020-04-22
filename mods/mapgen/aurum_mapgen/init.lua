6982b186c55ef42bff69255d43859697c65e4bfa
Beha <shacknetisp@mail.com>
2020-04-22 09:17:28 -0400

    Add river water aliases.

--- mods/mapgen/aurum_mapgen/init.lua
+++ mods/mapgen/aurum_mapgen/init.lua
@@ -2,3 +2,5 @@
 minetest.register_alias("mapgen_stone", "aurum_base:stone")
 minetest.register_alias("mapgen_water_source", "aurum_base:water_source")
 minetest.register_alias("mapgen_water_flowing", "aurum_base:water_flowing")
+minetest.register_alias("mapgen_river_water_source", "aurum_base:river_water_source")
+minetest.register_alias("mapgen_river_water_flowing", "aurum_base:river_water_flowing")

96f23a04789656b0d5dbae459e7a351d771aca27
Beha <shacknetisp@mail.com>
2020-09-14 11:47:25 -0400

    Add tumbleweed.

---
+++ mods/entities/aurum_mobs_env/mobs/tumbleweed.lua
@@ -0,0 +1,75 @@
+local S = minetest.get_translator()
+
+aurum.mobs.register("aurum_mobs_env:tumbleweed", {
+	description = S"Tumbleweed",
+	longdesc = S"A weed floating stoically past on the wind.",
+
+	initial_properties = {
+		visual = "sprite",
+		textures = {"aurum_mobs_env_tumbleweed.png"},
+		visual_size = {x = 0.6, y = 0.6},
+
+		hp_max = 1,
+	},
+
+	initial_data = {
+		habitat_nodes = {"group:sand", "group:clay"},
+		xmana = 1,
+		drops = {"aurum_flora:desert_weed"},
+		regen_rate = -1 / 60,
+	},
+
+	gemai = {
+		global_actions = {
+			"aurum_mobs:physics",
+			"aurum_mobs:environment",
+			"aurum_mobs:regen",
+		},
+
+		global_events = {
+			stuck = "roam",
+			timeout = "roam",
+			punch = "",
+			lost = "roam",
+			interact = "",
+			herd_alerted = "",
+		},
+
+		states = {
+			init = {
+				events = {
+					init = "roam",
+				},
+			},
+
+			roam = {
+				actions = {
+					"aurum_mobs:find_habitat",
+					"aurum_mobs:find_random",
+				},
+
+				events = {
+					found_habitat = "go",
+					found_random = "go",
+				},
+			},
+
+			go = {
+				actions = {
+					"aurum_mobs:go",
+					"aurum_mobs:timeout",
+				},
+
+				events = {
+					reached = "roam",
+				},
+			},
+		},
+	},
+})
+
+aurum.mobs.register_spawn{
+	mob = "aurum_mobs_env:tumbleweed",
+	chance = 11 ^ 3,
+	biomes = aurum.biomes.get_all_group("desert"),
+}
--- mods/entities/aurum_npcs/actions/summon.lua
+++ mods/entities/aurum_npcs/actions/summon.lua
@@ -1,10 +1,19 @@
 aurum.mobs.initial_data.summon = {
+	-- Mob to summon.
 	mob = "aurum_mobs_monsters:psyche_flare",
+	-- Summon one mob every <time> seconds.
 	time = 1,
+	-- Nearby players make summoning faster by this factor times the number of players.
+	-- The whole factor has a lower limit of 1 (no speedup).
+	nearby_players_factor = 0,
 }
 
 gemai.register_action("aurum_npcs:summon", function(self)
-	if math.random() * self.data.summon.time < self.data.step_time then
+	local npf = 1
+	if self.data.summon.nearby_players_factor ~= 0 then
+		math.max(1, self.data.summon.nearby_players_factor * aurum.mobs.helper_nearby_players(self))
+	end
+	if math.random() * self.data.summon.time / npf < self.data.step_time then
 		local object = aurum.mobs.spawn(self.entity.object:get_pos(), self.data.summon.mob)
 		if object then
 			local data = object:get_luaentity()._data.gemai

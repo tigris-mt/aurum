cd96ab9a5078ec595aa416f668fff6efb79628bc
Beha <shacknetisp@mail.com>
2020-10-18 18:01:57 -0400

    Add painted fish.

--- mods/entities/aurum_mobs_animals/mobs/painted_fish.lua
+++ mods/entities/aurum_mobs_animals/mobs/painted_fish.lua
@@ -1,12 +1,12 @@
 local S = minetest.get_translator()
 
-aurum.mobs.register("aurum_mobs_animals:angler", {
-	description = S"Angler",
-	longdesc = S"A uniquely Aurum creature, this fish inhabits cold waters in that realm.\nSpecimens that live underground are uniquely powerful and hostile.",
+aurum.mobs.register("aurum_mobs_animals:painted_fish", {
+	description = S"Painted Fish",
+	longdesc = S"A fantastic fish that loves living in the warmer waters of all Aurum.",
 
 	initial_properties = {
 		visual = "sprite",
-		textures = {"aurum_mobs_animals_angler.png"},
+		textures = {"aurum_mobs_animals_painted_fish.png"},
 		visual_size = {x = 1, y = 1},
 
 		hp_max = 15,
@@ -17,10 +17,10 @@ aurum.mobs.register("aurum_mobs_animals:angler", {
 	initial_data = {
 		habitat_nodes = {"group:water"},
 		drops = {"aurum_animals:raw_meat 2", "aurum_animals:bone 2"},
-		xmana = 6,
+		xmana = 2,
 		movement = "swim",
 		attack = b.t.combine(aurum.mobs.initial_data.attack, {
-			damage = {pierce = 8},
+			damage = {pierce = 2},
 		}),
 		base_speed = 2,
 	},
@@ -29,16 +29,7 @@ aurum.mobs.register("aurum_mobs_animals:angler", {
 
 	entity_def = {
 		_mob_init = function(self)
-			if self.object:get_pos().y < -50 then
-				self._data.properties = {hp_max = 30, textures = {"aurum_mobs_animals_angler_cave.png"}}
-				self._data.hp = 30
-				self._data.gemai.xmana = 14
-				self._data.gemai.attack = b.t.combine(aurum.mobs.initial_data.attack, {
-					damage = {pierce = 16},
-				})
-			else
-				self._data.gemai.hunt_prey = {""}
-			end
+			self._data.properties = {textures = {"aurum_mobs_animals_painted_fish.png^(aurum_mobs_animals_painted_fish_colors.png^[colorize:" .. b.color.tostring(b.t.choice(b.t.keys(b.color.names))) .. ":127)"}}
 		end,
 	},
 
@@ -66,13 +57,11 @@ aurum.mobs.register("aurum_mobs_animals:angler", {
 
 			roam = {
 				actions = {
-					"aurum_mobs:find_prey",
 					"aurum_mobs:find_habitat",
 					"aurum_mobs:find_random",
 				},
 
 				events = {
-					found_prey = "go_fight",
 					found_habitat = "go",
 					found_random = "go",
 				},
@@ -80,12 +69,7 @@ aurum.mobs.register("aurum_mobs_animals:angler", {
 
 			stand = {
 				actions = {
-					"aurum_mobs:find_prey",
 					"aurum_mobs:timeout",
-				},
-
-				events = {
-					found_prey = "go_fight",
 				},
 			},
 
@@ -125,8 +109,8 @@ aurum.mobs.register("aurum_mobs_animals:angler", {
 })
 
 aurum.mobs.register_spawn{
-	mob = "aurum_mobs_animals:angler",
+	mob = "aurum_mobs_animals:painted_fish",
 	chance = 20 ^ 3,
-	-- Aurum biomes with heat <= 50.
-	biomes = b.set.to_array(b.set._and(b.set(aurum.biomes.find(function(def) return def.heat_point <= 50 end)), b.set(aurum.biomes.get_all_group("aurum:aurum")))),
+	-- Aurum biomes with heat > 50.
+	biomes = b.set.to_array(b.set._and(b.set(aurum.biomes.find(function(def) return def.heat_point > 50 end)), b.set(aurum.biomes.get_all_group("aurum:aurum")))),
 }

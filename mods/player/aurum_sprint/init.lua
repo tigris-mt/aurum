70aad8cdd691363f49412256fd78150ee77df8f7
Beha <shacknetisp@mail.com>
2020-10-26 13:44:46 -0400

    Reduce hunger usage of sprinting.

--- mods/player/aurum_sprint/init.lua
+++ mods/player/aurum_sprint/init.lua
@@ -2,6 +2,6 @@ minetest.register_globalstep(function()
 	for _,player in ipairs(minetest.get_connected_players()) do
 		local sprint = player:get_player_control().aux1 and not player:get_attach()
 		player_monoids.speed:add_change(player, sprint and 2 or 1, "aurum_sprint:sprint")
-		aurum.hunger.LOSS:add_change(player, sprint and 15 or 1, "aurum_sprint:sprint")
+		aurum.hunger.LOSS:add_change(player, sprint and 10 or 1, "aurum_sprint:sprint")
 	end
 end)

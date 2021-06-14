130c9c3a56364f2ba997db5ee318375a7b2c5f8f
Beha <shacknetisp@mail.com>
2021-06-14 14:16:51 -0400

    Fix deep copying in tool enchantments.

--- mods/items/aurum_enchants/tools.lua
+++ mods/items/aurum_enchants/tools.lua
@@ -3,7 +3,8 @@ local S = aurum.get_translator()
 -- If the stack is a tool, copy tool_capabilities into the state and out into the meta.
 aurum.tools.register_enchant_callback{
 	init = function(state, stack)
-		state.tool_capabilities = stack:get_definition().tool_capabilities and table.copy(stack:get_definition().tool_capabilities)
+		local caps = stack:get_definition().tool_capabilities
+		state.tool_capabilities = caps and b.t.deep_copy(stack:get_definition().tool_capabilities)
 	end,
 
 	apply = function(state, stack)

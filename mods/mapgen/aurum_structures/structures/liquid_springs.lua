170dbc69080c0479b12118770107e10a21ccbc59
Beha <shacknetisp@mail.com>
2021-06-15 18:46:51 -0400

    Adjust liquid springs.

--- mods/mapgen/aurum_structures/structures/liquid_springs.lua
+++ mods/mapgen/aurum_structures/structures/liquid_springs.lua
@@ -1,10 +1,10 @@
 for _,d in ipairs{
-	{24, b.WORLD.max.y, 200},
-	{22, 200, 50},
+	{30, b.WORLD.max.y, 200},
+	{25, 200, 50},
 	{21, 50, 15},
-	{19, 15, -50},
-	{22, -50, -200},
-	{24, -200, b.WORLD.min.y},
+	{18, 15, -50},
+	{25, -50, -200},
+	{30, -200, b.WORLD.min.y},
 } do
 	minetest.register_ore({
 		ore_type = "scatter",
@@ -14,7 +14,7 @@ for _,d in ipairs{
 			b.set(aurum.biomes.find(function() return true end)),
 			b.set(aurum.biomes.get_all_group("frozen", {"base"}))
 		)),
-		wherein = {"aurum_base:stone", "group:soil", "aurum_base:gravel", "group:clay"},
+		wherein = {"aurum_base:stone", "aurum_base:gravel", "group:clay"},
 		clust_scarcity = d[1] ^ 3,
 		clust_num_ores = 1,
 		clust_size = 1,
@@ -24,13 +24,13 @@ for _,d in ipairs{
 end
 
 for _,d in ipairs{
-	{35, b.WORLD.max.y, 200},
-	{30, 200, 50},
-	{25, 50, 25},
-	{21, 25, 5},
-	{17, 5, -25},
-	{16, -25, -200},
-	{15, -200, b.WORLD.min.y},
+	{50, b.WORLD.max.y, 200},
+	{45, 200, 50},
+	{40, 50, 25},
+	{35, 25, 10},
+	{30, 10, -25},
+	{27, -25, -200},
+	{25, -200, b.WORLD.min.y},
 } do
 	minetest.register_ore({
 		ore_type = "scatter",
@@ -41,7 +41,7 @@ for _,d in ipairs{
 			b.set(aurum.biomes.get_all_group("forest", {"base"})),
 			b.set(aurum.biomes.get_all_group("frozen", {"base"}))
 		)),
-		wherein = {"aurum_base:stone", "group:soil", "aurum_base:gravel", "group:sand", "group:clay"},
+		wherein = {"aurum_base:stone", "aurum_base:gravel", "group:sand", "group:clay"},
 		clust_scarcity = d[1] ^ 3,
 		clust_num_ores = 1,
 		clust_size = 1,

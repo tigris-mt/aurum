a344d8f0c1583f35b59a67fb573e6c4caa6935d0
Beha <shacknetisp@mail.com>
2021-06-26 15:02:01 -0400

    Reduce power of breath enchant.

--- mods/entities/aurum_effects/effects/breath_regen.lua
+++ mods/entities/aurum_effects/effects/breath_regen.lua
@@ -1,13 +1,13 @@
 local S = aurum.get_translator()
 
 aurum.effects.register("aurum_effects:breath_regen", {
-	max_level = 12,
+	max_level = 3,
 	description = S"Breath Regen",
 	longdesc = S"Regenerates breath even when submerged.",
 	repeat_interval = 1,
 	enchant = false,
 	apply = function(object, level)
-		object:set_breath(object:get_breath() + b.random_whole(0.25 * level))
+		object:set_breath(object:get_breath() + b.random_whole(0.2 * level))
 	end,
 	cancel = function() end,
 })
